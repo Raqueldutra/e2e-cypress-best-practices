1d8e2ecc375b7223207f09adb334827626232db9
Andrey Oliveira <andreyoliveira@IN0440--Andrey-Oliveira--Engineering.local>
2023-10-31 12:36:25 -0300

    First setup for cy-step and add a Simple example test

--- cypress/support/types/selectors.d.ts
+++ cypress/support/types/selectors.d.ts
@@ -1,13 +1,19 @@
-export type Placeholders =
-	| "Placeholder";
+export type Placeholders = "Placeholder" | "What needs to be done?";
 
-export type Text =
-	|"text";
+export type Text = "text" | "Pay electric bill";
 
-export type DataTestId =
-	| "DataTestId";
+export type DataTestId = "DataTestId";
 
-export type Role = "button" | "td" | "tr" | "p" | "checkbox" | "a" | "span";
+export type Role =
+  | "button"
+  | "td"
+  | "tr"
+  | "p"
+  | "checkbox"
+  | "ul"
+  | "a"
+  | "span"
+  | "li"
+  | "class";
 
-export type Name =
-	| "name";
+export type Name = "name" | "todo-list";
